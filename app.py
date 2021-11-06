85ea1e8badb23916dbaddca1d84a3c785e34fba2
AmylesRH <88393013+AmylesRH@users.noreply.github.com>
2021-11-06 08:02:10 -0400

    update

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def main():
+    """Entry point; the view for the main page"""
+    return render_template('main.html')
+
+@app.route('/productview')
+def productview():
+    """The view for the Product Views page"""
+    return render_template('productview.html')
+
+@app.route('/contenttype')
+def contenttype():
+    """The view for the Content Type page"""
+    return render_template('contenttype.html')
+
+@app.route('/pageview')
+def pageview():
+    """The view for the Page Views page"""
+    return render_template('pageview.html')
 
 
 if __name__ == '__main__':
